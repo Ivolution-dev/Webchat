de74ba0654ec2e7fb000690e881f33a9bcc26120
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-22 16:00:06 +0100

    e

--- chat.php
+++ chat.php
@@ -19,8 +19,8 @@
 
         <table id="chat">
         <tr>
-        <th>Username</th>
-        <th>Message</th>
+        <th id="viertel">Username</th>
+        <th id="dreiviertel">Message</th>
         </tr>
         </table> 
             
