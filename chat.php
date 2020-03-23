48b7bdc8f3de9871119336d6976e022b1f7e264a
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-23 20:36:41 +0100

    f

--- chat.php
+++ chat.php
@@ -16,8 +16,8 @@
                 echo("<h1>Willkommen ".$_SESSION['username']."</h1>");
             }
         ?>
-        
-        <div class="logout">
+
+        <div class="feld">
             <form action="logout.php" method="post">
                 <input  type="submit" value="Logout" name="logout">
             </form>
