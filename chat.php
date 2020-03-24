9dba2a635014d8ddd07491a0b8d1683adc2c0d39
Ivolution-debug <62506790+Ivolution-debug@users.noreply.github.com>
2020-03-24 14:57:38 +0100

    Update chat.php

--- chat.php
+++ chat.php
@@ -30,10 +30,6 @@
         </table>
         <div id="scroll">
         <table id="chat">
-        <tr>
-        <th id="chatus"></th>
-        <th id="chatmsg"></th>
-        </tr>
         </table>
         </div><br>    
         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
