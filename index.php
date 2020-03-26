5e3255fabec17970d23773015bb717c992786a23
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-26 19:21:49 +0100

    d

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                     Nutzername oder Email  : <input type="text" name="nutzer" autofocus autocomplete="off"><br><br>
                     Passwort    : <input type="password" name="passwort" autocomplete="off"><br><br>
                     <input type="submit" value="Anmelden" name="login">
-                    <a href="account/create.php">Noch keinen Account? Hier registrieren!</a>
+                    <a href="account/create.php">Noch keinen Account? Hier klicken!</a>
                 </form>
             </div>
 
