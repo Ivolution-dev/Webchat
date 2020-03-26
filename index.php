9a6f7c76520e675d7b6f2a93417b4daff1bb3e75
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-26 19:27:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                 <form action="account/login.php" method="post"> 
                     Nutzername oder Email  : <input type="text" name="nutzer" autofocus autocomplete="off"><br><br>
                     Passwort    : <input type="password" name="passwort" autocomplete="off"><br><br>
-                    <input type="submit" value="Anmelden" name="login">
+                    <input type="submit" value="Anmelden" name="login"><br>
                     <a href="account/create.php">Noch keinen Account? Hier klicken!</a>
                 </form>
             </div>
