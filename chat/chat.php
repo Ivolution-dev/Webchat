bbf46d3517bac0e8483cb7be2803ff6a481e8464
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-26 17:01:05 +0100

    Update chat.php

--- chat/chat.php
+++ chat/chat.php
@@ -3,7 +3,7 @@
         <head>
             <title>Webchat</title> 
             <meta charset="utf-8">
-            <link rel="stylesheet" href="../style.css?v=5">
+            <link rel="stylesheet" href="../style.css?v=6">
             <link rel="icon" type="image/png" href="../logo.png">
         </head>
     <body>
