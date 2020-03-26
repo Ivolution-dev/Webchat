407c23752f75e7cb24352b1edbb2a8507da1c1ed
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-26 20:45:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                 <form action="account/login.php" method="post"> 
                     Nutzername oder Email  : <input type="text" name="nutzer" autofocus autocomplete="off"><br><br>
                     Passwort    : <input type="password" name="passwort" autocomplete="off">
-                    <a class="link" href="account/forgetpassword.php">Passwort vergessen?</a>
+                    <a class="link" href="account/forgetpassword.php">Passwort vergessen?</a><br><br>
                     <input type="submit" value="Anmelden" name="login">
                     <a class="link" href="account/create.php">Noch keinen Account? Hier klicken!</a>
                     
