aab14ebe7113f4e9c4e548eb7192cfb0bfdf125b
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-27 12:45:58 +0100

    w

--- account/changepassword.php
+++ account/changepassword.php
@@ -4,7 +4,7 @@
 <head>
     <title>Webchat</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../style.css?v=1">
+    <link rel="stylesheet" href="../style.css?v=2">
     <link rel="icon" type="image/png" href="../logo.png">
 </head>
 
