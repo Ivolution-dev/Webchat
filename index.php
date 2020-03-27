48122ece2f445342cb8cc03d64a79b901398ee40
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-27 23:25:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <title>Webchat</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../ressources/style.css?<?=filemtime('../ressources/style.css');?>"/>
+    <link rel="stylesheet" href="../ressources/style.css?<?=filemtime('../ressources/style.css');?>" />
     <link rel="icon" type="image/png" href="ressources/logo.png">
 </head>
 
