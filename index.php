83af7e922b3c4a0f5a7d3a6a681b829372ce88e0
Ivolution-debug <62506790+Ivolution-debug@users.noreply.github.com>
2020-03-30 13:44:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <title>Webchat</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="ressources/style.css?v=<? echo date('his'); ?>">
+    <link rel="stylesheet" type="text/css" href="ressources/style.css?<?php echo date('l jS \of F Y h:i:s A'); ?>" />
     <link rel="icon" type="image/png" href="ressources/logo.png">
 </head>
 
