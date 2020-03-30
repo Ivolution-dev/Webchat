a90099ed8a1e50688c27faab57d2f6ad5d4c933f
Ivolution-debug <62506790+Ivolution-debug@users.noreply.github.com>
2020-03-30 23:30:28 +0200

    Update profile.php

--- account/profile.php
+++ account/profile.php
@@ -11,8 +11,8 @@
 <body>
     <?php
     session_start();
-    if (isset($_SESSION['username'])) {
-        header('location: chat/chat.php');
+    if (!isset($_SESSION['username'])) {
+        header('location: ../index.php');
         exit();
     }
 
