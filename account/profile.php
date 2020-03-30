ad7b5138b9903e1d79239cbaf39845df43a50d7e
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-03-30 23:37:12 +0200

    Update profile.php

--- account/profile.php
+++ account/profile.php
@@ -21,11 +21,11 @@
     <h1 id="loghd"> Mein Profil </h1>
     <div class="feld">
         <form action="components/login.php" method="post">
-            Nutzername oder Email : <input type="text" name="nutzer" autofocus autocomplete="off"><br><br>
-            Passwort : <input type="password" name="passwort" autocomplete="off">
+            Nutzername oder Email : 
+            <?php
+                echo ("<p class=username>Du bist eingeloggt als: " . $_SESSION['username'] . "</p>");
+            ?>
             <a class="link" href="account/forgetpassword.php">Passwort vergessen?</a><br><br>
-            <input type="submit" value="Anmelden" name="login">
-            <a class="link" href="account/create.php">Noch keinen Account? Hier klicken!</a>
 
         </form>
     </div>
