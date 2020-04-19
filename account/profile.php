b35f665837f5f2332852d47f05f131d1ef7ea267
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-04-19 18:10:53 +0200

    s

--- account/profile.php
+++ account/profile.php
@@ -28,23 +28,23 @@
     <div class="feld">
         <form action="components/login.php" method="post">
             Nutzername :
-            <?php echo ("<p class=unp>" . $_SESSION['username'] . "</p>");?> 
+            <?php echo ("<p class=unp>" . $_SESSION['username'] . "</p>"); ?>
             <img src="../components/getprofilepicture.php?profile=<?php echo $_SESSION['username']; ?>" id="pbpic"></img><br>
             <form action="../components/upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="datei"><br><br>
-        <input type="submit" value="Hochladen"><br><br>
-            E-Mail :
-            <?php echo ("<p class=unp>" . $_SESSION['email'] . "</p>"); ?>         
+                <input type="file" name="datei"><br><br>
+                <input type="submit" value="Hochladen"><br><br>
+                E-Mail :
+                <?php echo ("<p class=unp>" . $_SESSION['email'] . "</p>"); ?>
+            </form>
         </form>
-    </form>  
-    
-    <form action="../components/passch.php" method="post">
-        Altes Passwort : <input type="password" name="oldpw" autocomplete="off"><br>
-        Neues Passwort : <input type="password" name="newpw" autocomplete="off"><br>
-        Neues Passwort bestätigen : <input type="password" name="newpwcn" autocomplete="off"><br><br>
-        <input type="submit" value="Passwort ändern!" name="change">
-        <a class="link" href="../index.php">Zurück zum Chat!</a>
-    </form>
+
+        <form action="../components/passch.php" method="post">
+            Altes Passwort : <input type="password" name="oldpw" autocomplete="off"><br>
+            Neues Passwort : <input type="password" name="newpw" autocomplete="off"><br>
+            Neues Passwort bestätigen : <input type="password" name="newpwcn" autocomplete="off"><br><br>
+            <input type="submit" value="Passwort ändern!" name="change">
+            <a class="link" href="../index.php">Zurück zum Chat!</a>
+        </form>
     </div>
     <?php
     if (isset($_SESSION['codeChangePassword']) && $_SESSION['codeChangePassword'] != "") {
