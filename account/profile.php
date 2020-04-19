e62cfb6eb3c84f54e1eb7f2804102ab3c5b8d973
Ivolution-debug <62506790+Ivolution-debug@users.noreply.github.com>
2020-04-19 17:08:59 +0200

    Update profile.php

--- account/profile.php
+++ account/profile.php
@@ -17,6 +17,7 @@
     }
     ?>
 
+    <img src = "../components/getprofilepicture.php?profile=<?php echo $_SESSION['username']; ?>"></img>
     <form action="../components/upload.php" method="post" enctype="multipart/form-data">
         <input type="file" name="datei"><br>
         <input type="submit" value="Hochladen">
