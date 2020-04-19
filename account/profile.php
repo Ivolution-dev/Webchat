a82d97a4c06fe2c89c1305b083cfdd3d698a1bc6
JustSnapz <62506843+JustSnapz@users.noreply.github.com>
2020-04-19 18:58:59 +0200

    Update profile.php

--- account/profile.php
+++ account/profile.php
@@ -26,17 +26,21 @@
 
     <h1 id="loghd"> Mein Profil </h1>
     <div class="feld">
-
-        <label for="name">Nutzername : <?php echo $_SESSION['username']; ?></label>
-        <img src="../components/getprofilepicture.php?profile=<?php echo $_SESSION['username']; ?>" id="pbpic"></img>
-        <form action="../components/upload.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="datei">
-            <input type="submit" value="Hochladen">
-            <label for="name"> E-Mail : <?php echo  $_SESSION['email'] ?></label>
+        <form action="components/login.php" method="post">
+            Nutzername :
+            <?php echo ("<p class=unp>" . $_SESSION['username'] . "</p>"); ?>
+            <img src="../components/getprofilepicture.php?profile=<?php echo $_SESSION['username']; ?>" id="pbpic"></img><br>
+            <form action="../components/upload.php" method="post" enctype="multipart/form-data">
+                <input type="file" name="datei"><br><br>
+                <input type="submit" value="Hochladen"><br><br>
+                E-Mail :
+                <?php echo ("<p class=unp>" . $_SESSION['email'] . "</p>"); ?>
+            </form>
         </form>
+
         <form action="../components/passch.php" method="post">
-            <div class="eins"> Altes Passwort : <input type="password" name="oldpw" autocomplete="off"></div><br>
-            <div class="eins">Neues Passwort : <input type="password" name="newpw" autocomplete="off"></div><br>
+            Altes Passwort : <input type="password" name="oldpw" autocomplete="off"><br>
+            Neues Passwort : <input type="password" name="newpw" autocomplete="off"><br>
             Neues Passwort bestätigen : <input type="password" name="newpwcn" autocomplete="off"><br><br>
             <input type="submit" value="Passwort ändern!" name="change">
             <a class="link" href="../index.php">Zurück zum Chat!</a>
