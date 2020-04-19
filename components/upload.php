865cf4220a76e8fa51d6d6826a00487a9bf3408f
Ivolution-debug <62506790+Ivolution-debug@users.noreply.github.com>
2020-04-19 14:13:57 +0200

    Update upload.php

--- components/upload.php
+++ components/upload.php
@@ -18,16 +18,6 @@
             die("Ungültige Dateiendung. Nur png, jpg, jpeg und gif-Dateien sind erlaubt");
         }
 
-        foreach ($allowed_extensions as &$al_extension) {
-            $file = $upload_folder.$filename.".".$al_extension;
-            echo $file;
-            if (file_exists($file)) {
-                echo "yes";
-                unlink($file);
-                break;
-            }
-        }
-
         //Überprüfung der Dateigröße
         $max_size = 1000 * 1024; //1000 KB
         if ($_FILES['datei']['size'] > $max_size) {
@@ -40,6 +30,14 @@
             $detected_type = exif_imagetype($_FILES['datei']['tmp_name']);
             if (!in_array($detected_type, $allowed_types)) {
                 die("Nur der Upload von Bilddateien ist gestattet");
+            }
+        }
+
+        foreach ($allowed_extensions as &$al_extension) {
+            $file = $upload_folder.$filename.".".$al_extension;
+            if (file_exists($file)) {
+                unlink($file);
+                break;
             }
         }
 
