af84c5d26f3a79d6481ffabe78c639e1d5d28578
Ivolution-debug <62506790+Ivolution-debug@users.noreply.github.com>
2020-04-19 14:11:11 +0200

    Update upload.php

--- components/upload.php
+++ components/upload.php
@@ -18,7 +18,7 @@
             die("Ungültige Dateiendung. Nur png, jpg, jpeg und gif-Dateien sind erlaubt");
         }
 
-        foreach ($allowed_extensions as $al_extensions) {
+        foreach ($allowed_extensions as &$al_extensions) {
             $file = $upload_folder.$filename.$al_extension;
             echo $file;
             if (file_exists($file)) {
